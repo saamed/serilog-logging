f2f292b315ad4ddb28a9f7bf19b7d38522ed9668
Bartosz Cłapa <bclapa@gmail.com>
2019-04-07 11:29:21 +0200

    saving in console

---
+++ Blog.Logging.Serilog.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Serilog;
+
+namespace Blog.Logging.Serilog.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger = log;
+
+            var a = 2;
+            var b = 0;
+
+            try
+            {
+                var c = a / b;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "@{Message} a = @{a} b = @{b}",e.Message, a, b);
+                //throw;
+            }
+        }
+    }
+}
